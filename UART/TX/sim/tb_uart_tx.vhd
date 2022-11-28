9894c96a979cdd2e20c895ed61f66e42a66e8882
efeyitim <62470610+efeyitim@users.noreply.github.com>
2022-11-28 20:48:53 +0300

    Add files via upload

--- UART/TX/sim/tb_uart_tx.vhd
+++ UART/TX/sim/tb_uart_tx.vhd
@@ -30,70 +30,82 @@ architecture tb of tb_uart_tx is
     constant BAUD_RATE     : integer := 115_200;
     constant NUM_DATA_BITS : integer := 8;
     constant NUM_STOP_BITS : integer := 1;
+    constant ENABLE_PARITY : boolean := false;
+    constant ODD_PARITY    : boolean := false;
 
     -- component ports
-    signal clk      : std_logic                     := '0';
-    signal rst_n    : std_logic                     := '0';
-    signal tx_din   : std_logic_vector (7 downto 0) := (others => '0');
-    signal tx_start : std_logic                     := '0';
+    signal clk      : std_logic                                     := '0';
+    signal rst_n    : std_logic                                     := '0';
+    signal tx_din   : std_logic_vector (NUM_DATA_BITS - 1 downto 0) := (others => '0');
+    signal tx_start : std_logic                                     := '0';
     signal tx       : std_logic;
     signal tx_busy  : std_logic;
     signal tx_done  : std_logic;
 
 
-    procedure waitNre(signal clock: std_ulogic; n: positive) is
+    procedure waitNre(signal clock : std_ulogic; n : positive) is
     begin
         for i in 1 to n loop
             wait until rising_edge(clock);
         end loop;
     end procedure waitNre;
 
+    procedure send_byte (
+        constant byte_in : in  std_logic_vector(NUM_DATA_BITS - 1 downto 0);
+        signal busy_tx   : in  std_logic;
+        signal din_tx    : out std_logic_vector(NUM_DATA_BITS - 1 downto 0);
+        signal start_tx  : out std_logic
+        ) is
+    begin
+        din_tx   <= byte_in;
+        start_tx <= '1';
+        wait until rising_edge(tx_busy);
+        start_tx <= '0';
+        wait until busy_tx = '0';
+    end send_byte;
+
 begin  -- architecture tb
 
     -- component instantiation
-    U_DUT: entity work.uart_tx
+    U_DUT : entity work.uart_tx
         generic map (
             CLK_FREQ      => CLK_FREQ,
             BAUD_RATE     => BAUD_RATE,
+            ENABLE_PARITY => ENABLE_PARITY,
+            ODD_PARITY    => ODD_PARITY,
             NUM_DATA_BITS => NUM_DATA_BITS,
             NUM_STOP_BITS => NUM_STOP_BITS)
         port map (
-            clk      => clk,
-            rst_n    => rst_n,
-            tx_din   => tx_din,
-            tx_start => tx_start,
-            tx       => tx,
-            tx_busy  => tx_busy,
-            tx_done  => tx_done);
+            clk           => clk,
+            rst_n         => rst_n,
+            tx_din        => tx_din,
+            tx_start      => tx_start,
+            tx            => tx,
+            tx_busy       => tx_busy,
+            tx_done       => tx_done);
 
     -- clock generation
     clk <= not clk after 10 ns;
 
     -- waveform generation
-    PROC_STIMULI: process
+    PROC_STIMULI : process
     begin
         -- insert signal assignments here
         rst_n <= '0';
         waitNre(clk, 5);
         rst_n <= '1';
         waitNre(clk, 100);
-        tx_din <= x"C3";
-        tx_start <= '1';
-        wait until rising_edge(tx_busy);
-        tx_start <= '0';
-        wait until tx_busy = '0';
-        tx_din <= x"F0";
-        tx_start <= '1';
-        wait until rising_edge(tx_busy);
-        tx_start <= '0';
-        wait until tx_busy = '0';
+        send_byte(x"A5", tx_busy, tx_din, tx_start);
+        send_byte(x"C3", tx_busy, tx_din, tx_start);
+        send_byte(x"F1", tx_busy, tx_din, tx_start);
+        send_byte(x"00", tx_busy, tx_din, tx_start);
         waitNre(clk, 100);
         report "SIM DONE" severity failure;
         wait;
     end process PROC_STIMULI;
-    
+
 
 end architecture tb;
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------
-    
+
--- UART/TX/src/uart_tx.vhd
+++ UART/TX/src/uart_tx.vhd
@@ -19,6 +19,8 @@ entity uart_tx is
     generic (
         CLK_FREQ      : integer := 50_000_000;
         BAUD_RATE     : integer := 115_200;
+        ENABLE_PARITY : boolean := false;
+        ODD_PARITY    : boolean := true;
         NUM_DATA_BITS : integer := 8;
         NUM_STOP_BITS : integer := 1
         );
@@ -26,7 +28,7 @@ entity uart_tx is
     port (
         clk      : in  std_logic;
         rst_n    : in  std_logic;
-        tx_din   : in  std_logic_vector (7 downto 0);
+        tx_din   : in  std_logic_vector (NUM_DATA_BITS - 1 downto 0);
         tx_start : in  std_logic;
         tx       : out std_logic;
         tx_busy  : out std_logic;
@@ -40,12 +42,27 @@ architecture Behavioral of uart_tx is
     constant STOP_BIT_TIME : unsigned(31 downto 0) := to_unsigned((CLK_FREQ / BAUD_RATE) * NUM_STOP_BITS, 32);
     constant DATA_BITS     : unsigned(3 downto 0)  := to_unsigned(NUM_DATA_BITS - 1, 4);
 
-    type t_state is (IDLE_S, START_S, STOP_S, DATA_S);
+    type t_state is (IDLE_S,                                          -- idle state
+                     START_S,                                         -- start bit
+                     STOP_S,                                          -- stop bit
+                     DATA_S,                                          -- data bits
+                     PARITY_S);                                       -- parity bit
     signal state : t_state;
 
     signal bit_timer   : unsigned(31 downto 0);
     signal bit_counter : unsigned(3 downto 0);
-    signal shift_reg   : std_logic_vector (7 downto 0);
+    signal shift_reg   : std_logic_vector (NUM_DATA_BITS - 1 downto 0);
+    signal data_parity : std_logic;
+
+    -- -2008 use unitary AND without parameter instead of call:
+    function reduce_xor (inp : std_logic_vector) return std_logic is
+        variable retval      : std_logic := '0';
+    begin
+        for i in inp'range loop
+            retval                       := retval xor inp(i);
+        end loop;
+        return retval;
+    end function;
 
 begin
 
@@ -58,9 +75,13 @@ begin
             tx_done     <= '0';
             tx          <= '1';
             tx_busy     <= '0';
+            data_parity <= '0';
             state       <= IDLE_S;
 
         elsif (rising_edge(clk)) then
+
+            data_parity <= reduce_xor(shift_reg);
+
             case state is
 
                 when IDLE_S =>
@@ -76,36 +97,59 @@ begin
 
                 when START_S =>
                     if (bit_timer = 0) then
-                        state                  <= DATA_S;
-                        tx                     <= shift_reg(0);
-                        shift_reg (7)          <= shift_reg(0);
-                        shift_reg (6 downto 0) <= shift_reg(7 downto 1);
-                        bit_timer              <= DATA_BIT_TIME;
+                        state                                   <= DATA_S;
+                        tx                                      <= shift_reg(0);
+                        shift_reg (shift_reg'high)              <= shift_reg(0);
+                        shift_reg (shift_reg'high - 1 downto 0) <= shift_reg(shift_reg'high downto 1);
+                        bit_timer                               <= DATA_BIT_TIME;
                     else
-                        bit_timer              <= bit_timer - 1;
+                        bit_timer                               <= bit_timer - 1;
                     end if;
 
                 when DATA_S =>
                     if (bit_counter = 0) then
                         if (bit_timer = 0) then
-                            state                  <= STOP_S;
-                            bit_counter            <= DATA_BITS;
-                            tx                     <= '1';
-                            bit_timer              <= STOP_BIT_TIME;
+                            bit_counter <= DATA_BITS;
+
+                            if ENABLE_PARITY then
+                                if ODD_PARITY then
+                                    tx                              <= not data_parity;
+                                end if;
+                                if not ODD_PARITY then
+                                    tx                              <= data_parity;
+                                end if;
+                                bit_timer                           <= DATA_BIT_TIME;
+                                state                               <= PARITY_S;
+                            end if;
+                            if not ENABLE_PARITY then
+                                tx                                  <= '1';
+                                bit_timer                           <= STOP_BIT_TIME;
+                                state                               <= STOP_S;
+                            end if;
                         else
-                            bit_timer              <= bit_timer - 1;
+                            bit_timer                               <= bit_timer - 1;
                         end if;
                     else
                         if (bit_timer = 0) then
-                            shift_reg (7)          <= shift_reg(0);
-                            shift_reg (6 downto 0) <= shift_reg(7 downto 1);
-                            tx                     <= shift_reg(0);
-                            bit_counter            <= bit_counter - 1;
-                            bit_timer              <= DATA_BIT_TIME;
+                            tx                                      <= shift_reg(0);
+                            shift_reg (shift_reg'high)              <= shift_reg(0);
+                            shift_reg (shift_reg'high - 1 downto 0) <= shift_reg(shift_reg'high downto 1);
+                            bit_counter                             <= bit_counter - 1;
+                            bit_timer                               <= DATA_BIT_TIME;
                         else
-                            bit_timer              <= bit_timer - 1;
+                            bit_timer                               <= bit_timer - 1;
                         end if;
                     end if;
+
+                when PARITY_S =>
+                    if (bit_timer = 0) then
+                        state     <= STOP_S;
+                        tx        <= '1';
+                        bit_timer <= STOP_BIT_TIME;
+                    else
+                        bit_timer <= bit_timer - 1;
+                    end if;
+
 
                 when STOP_S =>
                     if (bit_timer = 0) then
@@ -124,6 +168,7 @@ begin
                     tx_done     <= '0';
                     tx          <= '1';
                     tx_busy     <= '0';
+                    data_parity <= '0';
                     state       <= IDLE_S;
 
             end case;
